e67211bc313d7b2b99da4e288099666d8c0199c1
EvgenStr <juve2910@gmail.com>
2021-04-01 10:57:26 +0300

    format document

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,10 +1,10 @@
 'use strict';
 
-function Accumulator(startingValue=0) {
+function Accumulator(startingValue = 0) {
   if (!new.target) {
     new Accumulator(startingValue);
   }
-  this.value =isNaN(startingValue-1)? 0:startingValue;
+  this.value = isNaN(startingValue - 1) ? 0 : startingValue;
   // this.prototype = new ReadProto();
   // this.__proto__ = new ReadProto();
 }
